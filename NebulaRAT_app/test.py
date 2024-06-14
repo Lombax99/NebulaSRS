c00d870a5679c3986b40dad54af2c6f0df2621d4
Stefano Paparella <74765922+stef-a99@users.noreply.github.com>
2024-06-14 18:30:57 +0200

    test stampa query
    stamperà? chi lo sa!

--- NebulaRAT_app/test.py
+++ NebulaRAT_app/test.py
@@ -1,21 +1,27 @@
 import psycopg2
 cnx = psycopg2.connect(user="sudo", password="sudo", host="nebularat-postgresdb-server.postgres.database.azure.com", port=5432, database="nebularat-postgresServer-db")
-query = ("""
-INSERT INTO TEST (descrizione)
-VALUES ('palle');
+query = (#"""
+#INSERT INTO TEST (descrizione)
+#VALUES ('pislelo');
+#""",
+"""
+SELECT * FROM TEST;
 """)
 
 def test():
     try:
         with cnx as conn:
             with conn.cursor() as cur:
-                cur.execute(query)
+                #for q in query:
+                cur.execute(q)
+                machine_data = cur.fetchall()  # Fetch all rows at once
     except (psycopg2.DatabaseError, Exception) as error:
         print(error)
     finally:
         if cnx:
             cnx.close()
             print("Connessione al database chiusa")
+    return machine_data
 
 if __name__ == '__main__':
     test()
