e91039eed9d0cededef2e712c8be216b95bd6f7a
LucaLombardi <lombax99.ll@gmail.com>
2024-06-18 17:18:35 +0200

    Update app.py

--- NebulaRAT_app/app.py
+++ NebulaRAT_app/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, jsonify
 #from flask_sqlalchemy import SQLAlchemy
 #from flask_login import LoginManager
 from generateCertificate import *
-from test import *
+#from test import *
 
 
 app = Flask(__name__)
