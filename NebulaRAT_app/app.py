e24fe2c07d4c15432665b98b36ed64b3dac530ec
LucaLombardi <lombax99.ll@gmail.com>
2024-06-18 16:20:35 +0200

    aggiornamento requirements e nuove lib

--- NebulaRAT_app/app.py
+++ NebulaRAT_app/app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from generateCertificate import *
-from test import test
+from test import *
+
+
 app = Flask(__name__)
+
+
+
+
+
 
 @app.route('/')
 def root():
