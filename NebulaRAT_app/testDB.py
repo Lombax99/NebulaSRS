794139513c4a832c1bbf55d6f42d76ad58d4b21d
Stefano Paparella <74765922+stef-a99@users.noreply.github.com>
2024-06-21 14:30:29 +0200

    prova con psycopg
    ho provato a farlo come lo vuole azure, vediamo come va.

--- NebulaRAT_app/testDB.py
+++ NebulaRAT_app/testDB.py
@@ -1,33 +1,18 @@
-#import psycopg2
-#from flask import Flask, render_template
-#import json
-
-#cnx = psycopg2.connect(user="sudo", password="sudo", host="nebularat-postgresdb-server.postgres.database.azure.com", port=5432, database="nebularat-postgresServer-db")
-#query = ("""SELECT * FROM TEST;""")
+import psycopg2
+from flask import Flask, render_template
+import json
+from get_conn import get_connection_uri
 
 def prova():
-    '''cnx = psycopg2.connect(user="sudo", password="sudo", host="nebularat-postgresdb-server.postgres.database.azure.com", port=5432, database="nebularat-postgresServer-db")
-    query = ("""SELECT * FROM TEST;""")
-    #data_list = []
-    try:
-        with cnx as conn:
-            with conn.cursor() as cur:
-                #for q in query:
-                cur.execute(query)
-                machine_data = cur.fetchall()  # Fetch all rows at once
-                # Convert data to a JSON-friendly format
-                #for row in machine_data:
-                    #data_list.append(dict(zip([col.name for col in cur.description], row)))
-                
-    except (psycopg2.DatabaseError, Exception) as error:
-        print(error)
-    finally:
-        if cnx:
-            cnx.close()
-            print("Connessione al database chiusa")
+    conn_string = get_connection_uri()
 
-    return machine_data'''
-    return "Hello, world!"
-    
-#test()
-  
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM TEST;")
+    rows = cursor.fetchall()
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    return rows
