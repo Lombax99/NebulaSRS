85751b2faadd044b032fd036074c72ea163c46f1
Stefano Paparella <74765922+stef-a99@users.noreply.github.com>
2024-06-24 10:16:23 +0200

    Update app.py
    fix all conflicts

--- NebulaRAT_app/app.py
+++ NebulaRAT_app/app.py
@@ -2,51 +2,29 @@ from flask import Flask, render_template, jsonify
 #from flask_sqlalchemy import SQLAlchemy
 #from flask_login import LoginManager
 from generateCertificate import *
-<<<<<<< Updated upstream
-import psycopg2 as ps
-app = Flask(__name__)
 
-=======
-<<<<<<< HEAD
 import psycopg2
-
+cnx = psycopg2.connect(user="sudo", password="sudo", host="nebularat-postgresdb-server.postgres.database.azure.com", port=5432, database="nebularat-postgresServer-db")
+    
 app = Flask(__name__)
-
-db_connection = psycopg2.connect(
-    database="testdb",
-    user="Pasquale",
-    password="1999",
-    host="localhost"
-)
-=======
-import psycopg2 as ps
-app = Flask(__name__)
-
->>>>>>> Stashed changes
 
 def prova():
     query = "SELECT * FROM TEST;"
-    cnx = ps.connect(user="sudo", password="sudo", host="nebularat-postgresdb-server.postgres.database.azure.com", port=5432, database="nebularat-postgresServer-db")
     try:
         with cnx as conn:
             with conn.cursor() as cur:
                 cur.execute(query)
                 machine_data = cur.fetchall()  # Fetch all rows at once
                 return machine_data
-    except (ps.DatabaseError, Exception) as error:
+    except (psycopg2.DatabaseError, Exception) as error:
         print(error)
     finally:
         conn.close()
 
 
-<<<<<<< Updated upstream
-=======
->>>>>>> a79f6a3d03cb70ba931c3403211bd555b90c3420
->>>>>>> Stashed changes
-
 @app.route('/')
 def root():
-    cursor = db_connection.cursor()
+    cursor = cnx.cursor()
     cursor.execute("SELECT id, descrizione, indirizzoip FROM MACCHINA_Disp")
     macchine = cursor.fetchall()
     cursor.close()
@@ -93,9 +71,3 @@ def testPythonFunctionDB():
 
 if __name__ == '__main__':
    app.run(debug=True)
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
-
-
