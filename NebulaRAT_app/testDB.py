eaf33ff4aa7ad713759d93b9c2b17a29a50dbcfb
LucaLombardi <lombax99.ll@gmail.com>
2024-06-24 16:30:44 +0200

    Update testDB.py

--- NebulaRAT_app/testDB.py
+++ NebulaRAT_app/testDB.py
@@ -1,11 +1,12 @@
-from psycopg2 import connect
+#from psycopg2 import connect
 
 def testQuery():
-    conn = connect(user="sudo", password="sudo", host="nebularat-postgresdb-server.postgres.database.azure.com", port=5432, database="nebularat-postgresServer-db")
+    return "ciao"
+    '''conn = connect(user="sudo", password="sudo", host="nebularat-postgresdb-server.postgres.database.azure.com", port=5432, database="nebularat-postgresServer-db")
     cur = conn.cursor()                
     cur.execute("SELECT * FROM TEST;")
     machine_data = cur.fetchall()
     conn.commit() 
     cur.close()
     conn.close()
-    return machine_data
+    return machine_data'''
