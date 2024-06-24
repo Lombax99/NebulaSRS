d3fe0658961555f508792f6329d689df3a235e52
Stefano Paparella <74765922+stef-a99@users.noreply.github.com>
2024-06-24 14:27:42 +0200

    fix try #3

--- NebulaRAT_app/testDB.py
+++ NebulaRAT_app/testDB.py
@@ -1,11 +1,10 @@
 import urllib.parse
 import os
-import psycopg2
 
 def prova():
-    conn_string = "user=sudo, password=sudo, host=nebularat-postgresdb-server.postgres.database.azure.com, port=5432, database=nebularat-postgresServer-db"
+    import psycopg2
     try:
-        with psycopg2.connect(**conn_string) as conn:
+        with psycopg2.connect(user="sudo", password="sudo", host="nebularat-postgresdb-server.postgres.database.azure.com", port=5432, database="nebularat-postgresServer-db") as conn:
             with conn.cursor() as cur:
                 cur.execute("SELECT * FROM TEST;")
                 machine_data = cur.fetchall()
