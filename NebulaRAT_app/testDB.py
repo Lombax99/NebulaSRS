4c86db36614d4b843008dcd6711b8deefe2cdbb2
Stefano Paparella <74765922+stef-a99@users.noreply.github.com>
2024-06-24 14:17:24 +0200

    fix connection string #2

--- NebulaRAT_app/testDB.py
+++ NebulaRAT_app/testDB.py
@@ -1,10 +1,11 @@
 import urllib.parse
 import os
+import psycopg2
 
 def prova():
-    import psycopg2
+    conn_string = "user=sudo, password=sudo, host=nebularat-postgresdb-server.postgres.database.azure.com, port=5432, database=nebularat-postgresServer-db"
     try:
-        with psycopg2.connect(user="sudo", password="sudo", host="nebularat-postgresdb-server.postgres.database.azure.com", port=5432, database="nebularat-postgresServer-db") as conn:
+        with psycopg2.connect(**conn_string) as conn:
             with conn.cursor() as cur:
                 cur.execute("SELECT * FROM TEST;")
                 machine_data = cur.fetchall()
