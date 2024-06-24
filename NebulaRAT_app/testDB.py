da5158e50c095037679856a31999d97c001dbb65
Stefano Paparella <74765922+stef-a99@users.noreply.github.com>
2024-06-24 13:49:34 +0200

    Update testDB.py

--- NebulaRAT_app/testDB.py
+++ NebulaRAT_app/testDB.py
@@ -1,22 +1,10 @@
 import urllib.parse
 import os
 
-def get_connetion_uri():
-    dbhost = 'nebularat-postgresdb-server.postgres.database.azure.com'
-    dbname = 'nebularat-postgresServer-db'
-    dbuser = 'sudo'
-    password = 'sudo'
-    sslmode = 'require'
-
-    db_uri = f"postgresql://{dbuser}:{password}@{dbhost}/{dbname}?sslmode={sslmode}"
-
-    return db_uri
-
 def prova():
     import psycopg2
     try:
-        conn_string = get_connetion_uri()
-        with psycopg2.connect(**conn_string) as conn:
+        with psycopg2.connect(user="sudo", password="sudo", host="nebularat-postgresdb-server.postgres.database.azure.com", port=5432, database="postgres") as conn:
             with conn.cursor() as cursor:
                 cursor.execute("SELECT * FROM TEST;")
                 data = cursor.fetchall()
@@ -25,7 +13,7 @@ def prova():
     finally:
         cursor.close()
         conn.close()
-        
+
     return data
 
 if __name__ == '__main__':
