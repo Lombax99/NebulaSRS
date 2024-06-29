127e3f3fff2c9540203d6ff419f9e8ca691b5ca7
Stefano Paparella <74765922+stef-a99@users.noreply.github.com>
2024-06-29 12:53:22 +0200

    update query funzionanti

--- NebulaRAT_app/app.py
+++ NebulaRAT_app/app.py
@@ -1,5 +1,4 @@
 #from generateCertificate import *
-import string
 from settings import postgresql as settings
 from flask import Flask, render_template, request, jsonify
 from queryexe import execute_query
