084748cf0a9466d4a9ed96143674a1ed8677e175
LilPack99 <pasbasta1@gmail.com>
2024-07-16 12:54:37 +0200

    Update update_password_hashes.py

--- NebulaRAT_app/update_password_hashes.py
+++ NebulaRAT_app/update_password_hashes.py
@@ -1,17 +1,17 @@
 import json
 import bcrypt
 
-# Funzione per verificare se una password è già hashata
+# Function to check if a password is already hashed
 def is_hashed(password):
     return password.startswith('$2b$')
 
-# Funzione per generare l'hash della password
+# Function to generate password hash
 def generate_password_hash(password):
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password.decode('utf-8')
 
-# Funzione per aggiornare il file JSON con l'hash delle password
+# Function to update JSON file with password hash
 def update_json_with_password_hashes(json_file):
     with open(json_file, 'r+') as file:
         data = json.load(file)
