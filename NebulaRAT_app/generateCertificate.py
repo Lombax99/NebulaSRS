4408e3e176aeffb854082c9c7532e82640d77c67
LucaLombardi <lombax99.ll@gmail.com>
2024-07-18 17:07:16 +0200

    Update generateCertificate.py

--- NebulaRAT_app/generateCertificate.py
+++ NebulaRAT_app/generateCertificate.py
@@ -6,6 +6,8 @@ import os
 # "requiredIP" is the IP address of the machine that the certificate is being generated for
 # "duration" is the duration is the time for which the certificate is valid
 def generate_Certificate(username, requiredIP, duration):
+    requiredIP = requiredIP.split("/")[0] + "/24"
+    print(requiredIP)
     nebulaCert_path = os.path.join("nebulaScripts", "nebula-cert")
     outputDir = "nebulaFiles"
     # Check if the script exists
@@ -25,3 +27,7 @@ def generate_Certificate(username, requiredIP, duration):
         raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), nebulaCert_path)
     
     return os.path.join(outputDir, username + ".crt"), os.path.join(outputDir, username + ".key"), outputDir
+
+
+if __name__ == "__main__":
+    print(generate_Certificate("test", "192.168.100.121/32", "8h"))
